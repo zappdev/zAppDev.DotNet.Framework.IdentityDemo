c5017c019c2c8c856c8fdbd0af87577d9692b36d
Bill <bill_vasilopoulos@hotmail.com>
2019-09-30 17:00:39 +0300

    Create Admin Controller

---
+++ IdentityDemo/Controllers/CreateAdminController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+using IdentityDemo.DTOs;
+using IdentityDemo.DAL;
+
+namespace IdentityDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateAdminController : ControllerBase
+    {
+        private ISession _session { get; set; }
+        public CreateAdminController(ISession session)
+        {
+            _session = session;
+        }
+
+        [HttpPost]
+        public ActionResult<zAppDev.DotNet.Framework.Identity.Model.ApplicationUser> CreateAdmin(ApplicationUserDTO userDTO)
+        {
+            if (userDTO.password?.Trim() != userDTO.passwordRepeat?.Trim())
+            {
+                throw new Exception("Passwords do not match!");
+            }
+            if (userDTO.username?.Trim() == "")
+            {
+                throw new Exception("No username provided!");
+            }
+            zAppDev.DotNet.Framework.Identity.Model.ApplicationRole adminRole = new Repository(_session).GetAsQueryable<zAppDev.DotNet.Framework.Identity.Model.ApplicationRole>((r) => r.Name == "Administrator")?.FirstOrDefault();
+            if ((adminRole == null))
+            {
+                throw new Exception("No Administrator role found in Database!");
+            }
+            zAppDev.DotNet.Framework.Identity.Model.ApplicationUser adminUser = new zAppDev.DotNet.Framework.Identity.Model.ApplicationUser();
+            adminUser.UserName = (userDTO.username?.Trim() ?? "");
+            adminUser?.AddRoles(adminRole);
+            string possibleError = zAppDev.DotNet.Framework.Identity.IdentityHelper.CreateUser(adminUser, (userDTO.password?.Trim() ?? ""));
+            if ((((possibleError == null || possibleError == "")) == false))
+            {
+                throw new Exception("Something Went Wrong");
+            }
+            return CreatedAtAction("CreateAdmin", new { id = adminUser.UserName }, adminUser);
+        }
+    }
+}
---
+++ IdentityDemo/DTOs/ApplicationUserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDemo.DTOs
+{
+    public class ApplicationUserDTO
+    {
+        public string username { get; set; }
+        //public string passwordHash;
+        //public string securityStamp;
+        public bool emailConfirmed { get; set; }
+        public bool lockoutEnabled { get; set; }
+        public bool phoneNumberConfirmed { get; set; }
+        public bool twoFactorEnabled { get; set; }
+        public int? accessFailedCount { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public DateTime? lockoutEndDate { get; set; }
+        public byte[] versionTimestamp { get; set; }
+        public string password { get; set; }
+        public string passwordRepeat { get; set; }
+}
+}
